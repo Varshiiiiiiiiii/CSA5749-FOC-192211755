740a2c65f9d506f05063507daadd4e744313f6ff
VARSHITHA <123988665+Varshiiiiiiiiii@users.noreply.github.com>
2023-02-08 18:49:05 +0530

    Add files via upload

---
+++ exp 17 printing digits.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num = 1024;
+
+    while(num != 0){
+        int digit = num % 10;
+        num = num / 10;
+        printf("%d\n", digit);
+    }
+  
+    return 0;
+}
